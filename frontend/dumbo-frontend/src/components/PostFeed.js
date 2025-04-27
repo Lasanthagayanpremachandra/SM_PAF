1c35da4c11690e28cdccdcc5da6deaa95d6ceba3
sandeepaeshan1 <167762219+sandeepaeshan1@users.noreply.github.com>
2025-04-27 11:32:27 +0530

    Update PostFeed.js

--- frontend/dumbo-frontend/src/components/PostFeed.js
+++ frontend/dumbo-frontend/src/components/PostFeed.js
@@ -1,129 +1,233 @@
-import React, { useState, useEffect } from 'react';
-import { useApiProgress } from '../shared/ApiProgress';
-
-import { getPosts, getOldPosts, getNewPostCount, getNewPosts } from '../api/apiCalls';
+import React, { useEffect, useState } from 'react';
+import { useParams, useHistory } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 import { useTranslation } from 'react-i18next';
-import { useParams } from 'react-router-dom';
-import PostView from './PostView';
+import ProfileImageWithDefault from './ProfileImageWithDefault';
+import Input from './Input';
+import Modal from './Modal';
+import { updateUser, deleteUser } from '../api/apiCalls';
+import { useApiProgress } from '../shared/ApiProgress';
+import { updateSuccess, logoutSuccess } from '../redux/authActions';
 
-const PostFeed = () => {
-    const { t } = useTranslation();
-    const [postPage, setPostPage] = useState({ content: [], last: true, number: 0 });
-    const [newPostCount, setNewPostCount] = useState(0);
-    const { username } = useParams();
-    const path = username ? `/api/1.0/users/${username}/posts?page=` : '/api/1.0/posts?page=';
-    const initialPostLoadProgress = useApiProgress('get', path);
+const ProfileCard = (props) => {
+  const { t } = useTranslation();
+  const [inEditMode, setInEditMode] = useState(false);
+  const [updatedDisplayName, setUpdatedDisplayName] = useState();
+  const [editable, setEditable] = useState(false);
+  const [user, setUser] = useState({});
+  const [newProfileImage, setNewProfileImage] = useState();
+  const [validationErrors, setValidationErrors] = useState({});
+  const [modalVisible, setModalVisible] = useState(false);
 
-    let lastPostId = 0;
-    let firstPostId = 0;
+  const { username: loggedInUsername } = useSelector((store) => ({
+    username: store.username,
+  }));
+  const routeParams = useParams();
+  const pathUsername = routeParams.username;
+  const dispatch = useDispatch();
+  const history = useHistory();
 
-    if (postPage.content.length > 0) {
-        firstPostId = postPage.content[0].id;
-        const lastPostIndex = postPage.content.length - 1;
-        lastPostId = postPage.content[lastPostIndex].id;
+  const { username, displayName, profileImage } = user;
+
+  const pendingApiCall = useApiProgress('put', `/api/1.0/users/${username}`);
+  const pendingApiCallDeleteUser = useApiProgress('delete', `/api/1.0/users/${username}`, true);
+
+  const { displayName: displayNameError, profileImage: profileImageError } = validationErrors;
+
+  useEffect(() => {
+    setUser(props.user);
+  }, [props.user]);
+
+  useEffect(() => {
+    setEditable(pathUsername === loggedInUsername);
+  }, [pathUsername, loggedInUsername]);
+
+  useEffect(() => {
+    setValidationErrors((prevErrors) => ({
+      ...prevErrors,
+      displayName: undefined,
+    }));
+  }, [updatedDisplayName]);
+
+  useEffect(() => {
+    setValidationErrors((prevErrors) => ({
+      ...prevErrors,
+      profileImage: undefined,
+    }));
+  }, [newProfileImage]);
+
+  useEffect(() => {
+    if (!inEditMode) {
+      setUpdatedDisplayName(undefined);
+      setNewProfileImage(undefined);
+    } else {
+      setUpdatedDisplayName(displayName);
+    }
+  }, [inEditMode, displayName]);
+
+  const onClickSave = async () => {
+    let profileImageBase64;
+    if (newProfileImage) {
+      profileImageBase64 = newProfileImage.split(',')[1];
     }
 
-    const oldPostPath = username ? `/api/1.0/users/${username}/posts/${lastPostId}` : `/api/1.0/posts/${lastPostId}`;
-    const loadOldPostsProgress = useApiProgress('get', oldPostPath, true);
-
-    const newPostPath = username
-        ? `/api/1.0/users/${username}/posts/${firstPostId}?direction=after`
-        : `/api/1.0/posts/${firstPostId}?direction=after`;
-    const loadNewPostsProgress = useApiProgress('get', newPostPath, true);
-
-    useEffect(() => {
-        const getCount = async () => {
-            const response = await getNewPostCount(firstPostId, username);
-            setNewPostCount(response.data.count);
-        };
-        let looper = setInterval(getCount, 5000);
-        return function cleanup() {
-            clearInterval(looper);
-        };
-    }, [firstPostId, username]);
-
-    useEffect(() => {
-        const loadPosts = async (page) => {
-            try {
-                const response = await getPosts(username, page);
-                setPostPage(previousPostPage => ({
-                    ...response.data,
-                    content: [...previousPostPage.content, ...response.data.content]
-                }));
-            } catch (error) { }
-        };
-        loadPosts();
-    }, [username]);
-
-
-
-    const loadOldPosts = async () => {
-        const response = await getOldPosts(lastPostId, username);
-        setPostPage(previousPostPage => ({
-            ...response.data,
-            content: [...previousPostPage.content, ...response.data.content]
-        }));
+    const body = {
+      displayName: updatedDisplayName,
+      profileImage: profileImageBase64,
     };
 
-    const loadNewPosts = async () => {
-        const response = await getNewPosts(firstPostId, username);
-        setNewPostCount(0);
-        setPostPage(previousPostPage => ({
-            ...previousPostPage,
-            content: [...response.data, ...previousPostPage.content]
-        }));
+    try {
+      const response = await updateUser(username, body);
+      setInEditMode(false);
+      setUser(response.data);
+      dispatch(updateSuccess(response.data));
+    } catch (error) {
+      setValidationErrors(error.response.data.validationErrors);
+    }
+  };
 
+  const onChangeFile = (event) => {
+    const file = event.target.files[0];
+    const fileReader = new FileReader();
+    fileReader.onloadend = () => {
+      setNewProfileImage(fileReader.result);
     };
+    fileReader.readAsDataURL(file);
+  };
 
-    const onDeletePostSuccess = id => {
-        setPostPage(previousPostPage => ({
-            ...previousPostPage,
-            content: previousPostPage.content.filter(post => post.id !== id)
-        }));
-    };
+  const onClickCancel = () => {
+    setModalVisible(false);
+  };
 
-    const { content, last } = postPage;
+  const onClickDeleteUser = async () => {
+    await deleteUser(username);
+    setModalVisible(false);
+    dispatch(logoutSuccess());
+    history.push('/');
+  };
 
-    const LoadSpinner = () => {
-        return (
-            <div className="d-flex justify-content-center mb-2">
-                <div className="spinner-grow spinner-grow-sm text-primary m-2" role="status">
-                    <span className="visually-hidden">Loading...</span>
-                </div>
-                <div className="spinner-grow spinner-grow-sm text-primary m-2" role="status">
-                    <span className="visually-hidden">Loading...</span>
-                </div>
-                <div className="spinner-grow spinner-grow-sm text-primary m-2" role="status">
-                    <span className="visually-hidden">Loading...</span>
-                </div>
+  return (
+    <div className="container mt-3">
+      <div className="row d-flex justify-content-center">
+        <div className="col-md-10">
+          <div className="card profileCard p-3 py-4">
+            <div className="text-center">
+              <ProfileImageWithDefault
+                width="150"
+                height="150"
+                className="img-thumbnail rounded-circle"
+                alt={`${username} profile`}
+                profileimage={profileImage}
+                temp={newProfileImage}
+              />
             </div>
-        );
-    }
 
-    return (
-        <div>
-            {!(newPostCount === 0) && (
-                <div className="alert alert-info text-primary text-center"
-                    style={{ cursor: loadNewPostsProgress ? 'not-allowed' : 'pointer' }}
-                    onClick={loadNewPostsProgress ? () => { } : loadNewPosts}>
-                    {t('There are new posts!')}
+            {!inEditMode && (
+              <div className="text-center mt-3">
+                <span className="bg-primary p-1 px-4 rounded text-white">@{username}</span>
+                <h3 className="mt-2 mb-0">{displayName}</h3>
+                <span>Lorem Ipsum Dolor</span>
+                <div className="px-5 mt-1">
+                  <p className="fonts">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua.
+                  </p>
                 </div>
 
+                {editable && (
+                  <div className="buttons">
+                    <button
+                      className="btn btn-primary px-4"
+                      onClick={() => setInEditMode(true)}
+                      disabled={pendingApiCall}
+                    >
+                      {pendingApiCall && <span className="spinner-border spinner-border-sm"></span>}
+                      {t('Edit')}
+                      <img src="https://img.icons8.com/stickers/30/000000/sign-up.png" alt="Edit" />
+                    </button>
+
+                    <button
+                      className="btn btn-outline-danger px-4 ms-3"
+                      onClick={() => setModalVisible(true)}
+                    >
+                      {t('Delete')}
+                      <img
+                        src="https://img.icons8.com/stickers/30/000000/delete-forever.png"
+                        alt="Delete"
+                      />
+                    </button>
+                  </div>
+                )}
+              </div>
             )}
 
-            {loadNewPostsProgress && <LoadSpinner />}
+            {inEditMode && (
+              <div className="text-center mt-3">
+                <div className="mb-3">
+                  <input
+                    type="file"
+                    onChange={onChangeFile}
+                    className={profileImageError ? 'form-control is-invalid' : 'form-control mb-2'}
+                  />
+                  {profileImageError && (
+                    <div className="invalid-feedback">{profileImageError}</div>
+                  )}
+                </div>
 
-            {content.map(post => {
-                return <PostView key={post.id} post={post} onDeletePost={onDeletePostSuccess} />;
-            })}
+                <Input
+                  name="displayName"
+                  defaultValue={displayName}
+                  onChange={(e) => setUpdatedDisplayName(e.target.value)}
+                  label={t('Change Display Name')}
+                  placeholder="Display Name"
+                  error={displayNameError}
+                />
 
-            {(loadOldPostsProgress || initialPostLoadProgress) && < LoadSpinner />}
+                <div className="buttons">
+                  <button
+                    className="btn btn-primary px-4"
+                    onClick={onClickSave}
+                    disabled={pendingApiCall}
+                  >
+                    {pendingApiCall && <span className="spinner-border spinner-border-sm"></span>}
+                    {t('Save')}
+                    <img src="https://img.icons8.com/stickers/30/000000/inbox.png" alt="Save" />
+                  </button>
 
-            {!last && <div className="alert alert-info text-primary text-center" style={{ cursor: loadOldPostsProgress ? 'not-allowed' : 'pointer' }} onClick={loadOldPostsProgress ? () => { } : loadOldPosts}>{t('Show more posts!')}</div>}
-
-            {(last && !initialPostLoadProgress) && <div div className="alert alert-info text-primary text-center">{t('There are no posts here!')}</div>}
-
+                  <button
+                    className="btn btn-outline-danger px-4 ms-3"
+                    onClick={() => setInEditMode(false)}
+                    disabled={pendingApiCall}
+                  >
+                    {t('Cancel')}
+                    <img
+                      src="https://img.icons8.com/stickers/30/000000/delete-sign.png"
+                      alt="Cancel"
+                    />
+                  </button>
+                </div>
+              </div>
+            )}
+          </div>
         </div>
-    );
+      </div>
+
+      <Modal
+        visible={modalVisible}
+        okButton={t('Delete My Account')}
+        onClickCancel={onClickCancel}
+        onClickOk={onClickDeleteUser}
+        message={
+          <div className="text-center">
+            {t('Your account will be permanently deleted.')}
+            <br />
+            {t('Are you sure?')}
+          </div>
+        }
+        pendingApiCall={pendingApiCallDeleteUser}
+      />
+    </div>
+  );
 };
-export default PostFeed;
+
+export default ProfileCard;
